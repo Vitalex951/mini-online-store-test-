4ab970eec0ff088cf52f0ea569dfc6de02974de8
Vitalex <vitalex951@gmail.com>
2024-07-17 15:22:14 +0300

    added midleware and type dispatch

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import { productPageReducer } from './reducers/product-page';
+import {IProductPageRepository} from "../services/products/interfaces";
+import {MockProductPageGateway} from "../gateways/product-page";
+import {useDispatch} from "react-redux";
 
 export const rootReducer = combineReducers({
   productPage: productPageReducer,
@@ -9,8 +12,31 @@ export type RootState = ReturnType<typeof rootReducer>;
 
 const preloadedState: Partial<RootState> = {};
 
+export interface ThunkExtraArg {
+  api: IProductPageRepository
+}
+export interface ThunkConfig<T> {
+  rejectValue: T
+  state: RootState
+  extra: ThunkExtraArg
+}
+
+const extraArg: ThunkExtraArg = {
+  api: new MockProductPageGateway()
+};
+
+
 export const store = configureStore({
   reducer: rootReducer,
   preloadedState,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    thunk: {
+      extraArgument: extraArg,
+    },
+  }),
 });
-export const dispatch = store.dispatch;
+
+
+// затипизировали наш dispatch
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
